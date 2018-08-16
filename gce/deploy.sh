3d5729253839165aed83d2b647326fd8b7642c93
Neil <nmiu@google.com>
2018-08-16 13:42:38 +1000

    split deploy and expose

--- gce/deploy.sh
+++ gce/deploy.sh
@@ -24,7 +24,7 @@ MACHINE_TYPE=g1-small
 STARTUP_SCRIPT=startup-script.sh
 # IMAGE_FAMILY=ubuntu-1604-lts
 # IMAGE_PROJECT=ubuntu-os-cloud
-IMAGE_FAMILY=debian-8
+IMAGE_FAMILY=debian-9
 IMAGE_PROJECT=debian-cloud
 SCOPES="userinfo-email,\
 logging-write,\
@@ -78,85 +78,3 @@ gcloud compute instance-groups managed set-named-ports $GROUP \
   --zone $ZONE
 # [END create_named_port]
 
-#
-# Load Balancer Setup
-#
-
-# A complete HTTP load balancer is structured as follows:
-#
-# 1) A global forwarding rule directs incoming requests to a target HTTP proxy.
-# 2) The target HTTP proxy checks each request against a URL map to determine the
-#    appropriate backend service for the request.
-# 3) The backend service directs each request to an appropriate backend based on
-#    serving capacity, zone, and instance health of its attached backends. The
-#    health of each backend instance is verified using either a health check.
-#
-# We'll create these resources in reverse order:
-# service, health check, backend service, url map, proxy.
-
-# Create a health check
-# The load balancer will use this check to keep track of which instances to send traffic to.
-# Note that health checks will not cause the load balancer to shutdown any instances.
-
-# [START create_health_check]
-gcloud compute http-health-checks create $HC \
-  --request-path /_ah/health \
-  --port $PORT
-# [END create_health_check]
-
-# Create a backend service, associate it with the health check and instance group.
-# The backend service serves as a target for load balancing.
-
-# [START create_backend_service]
-gcloud compute backend-services create $SERVICE \
-  --global \
-  --http-health-checks $HC
-# [END create_backend_service]
-
-# [START add_backend_service]
-gcloud compute backend-services add-backend $SERVICE \
-  --global \
-  --instance-group $GROUP \
-  --instance-group-zone $ZONE
-# [END add_backend_service]
-
-# Create a URL map and web Proxy. The URL map will send all requests to the
-# backend service defined above.
-
-# [START create_url_map]
-gcloud compute url-maps create $SERVICE-map \
-  --default-service $SERVICE
-# [END create_url_map]
-
-# [START create_http_proxy]
-gcloud compute target-http-proxies create $SERVICE-proxy \
-  --url-map $SERVICE-map
-# [END create_http_proxy]
-
-# Create a global forwarding rule to send all traffic to our proxy
-
-# [START create_forwarding_rule]
-gcloud compute forwarding-rules create $SERVICE-http-rule \
-  --global \
-  --target-http-proxy $SERVICE-proxy \
-  --ports $PORT
-# [END create_forwarding_rule]
-
-#
-# Autoscaler configuration
-#
-# [START set_autoscaling]
-gcloud compute instance-groups managed set-autoscaling $GROUP \
-  --max-num-replicas $MAX_INSTANCES \
-  --cool-down-period $COOL_DOWN_PERIOD \
-  --target-load-balancing-utilization $TARGET_UTILIZATION \
-  --zone $ZONE
-# [END set_autoscaling]
-
-# [START create_firewall]
-gcloud compute firewall-rules create $SERVICE-allow-http-$PORT \
-  --allow tcp:$PORT \
-  --source-ranges 0.0.0.0/0 \
-  --target-tags $TAGS \
-  --description "Allow port $PORT access to $TAGS"
-# [END create_firewall]
